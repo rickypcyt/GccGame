2f5506853a978750520fc1552c5ff79985cbd79e
rickypcyt <105986682+rickypcyt@users.noreply.github.com>
2024-07-11 07:05:56 +0200

    2D Square Moveable

--- build/CMakeFiles/My3DGame.dir/src/renderer.cpp.o.d
+++ build/CMakeFiles/My3DGame.dir/src/renderer.cpp.o.d
@@ -1,8 +1,7 @@
 CMakeFiles/My3DGame.dir/src/renderer.cpp.o: \
  /home/ricky/Documents/Codeo2/GCCGAME/src/renderer.cpp \
- /usr/include/stdc-predef.h \
- /home/ricky/Documents/Codeo2/GCCGAME/src/../include/renderer.h \
- /usr/include/GL/glew.h /usr/include/c++/14.1.1/cstddef \
+ /usr/include/stdc-predef.h /usr/include/GL/glew.h \
+ /usr/include/c++/14.1.1/cstddef \
  /usr/include/c++/14.1.1/x86_64-pc-linux-gnu/bits/c++config.h \
  /usr/include/c++/14.1.1/x86_64-pc-linux-gnu/bits/os_defines.h \
  /usr/include/features.h /usr/include/features-time64.h \
@@ -142,23 +141,4 @@ CMakeFiles/My3DGame.dir/src/renderer.cpp.o: \
  /usr/include/c++/14.1.1/bits/locale_facets.tcc \
  /usr/include/c++/14.1.1/bits/basic_ios.tcc \
  /usr/include/c++/14.1.1/bits/ostream.tcc /usr/include/c++/14.1.1/istream \
- /usr/include/c++/14.1.1/bits/istream.tcc /usr/include/c++/14.1.1/cmath \
- /usr/include/math.h /usr/include/bits/math-vector.h \
- /usr/include/bits/libm-simd-decl-stubs.h \
- /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
- /usr/include/bits/fp-fast.h \
- /usr/include/bits/mathcalls-helper-functions.h \
- /usr/include/bits/mathcalls.h /usr/include/bits/mathcalls-narrow.h \
- /usr/include/bits/iscanonical.h /usr/include/c++/14.1.1/bits/specfun.h \
- /usr/include/c++/14.1.1/limits /usr/include/c++/14.1.1/tr1/gamma.tcc \
- /usr/include/c++/14.1.1/tr1/special_function_util.h \
- /usr/include/c++/14.1.1/tr1/bessel_function.tcc \
- /usr/include/c++/14.1.1/tr1/beta_function.tcc \
- /usr/include/c++/14.1.1/tr1/ell_integral.tcc \
- /usr/include/c++/14.1.1/tr1/exp_integral.tcc \
- /usr/include/c++/14.1.1/tr1/hypergeometric.tcc \
- /usr/include/c++/14.1.1/tr1/legendre_function.tcc \
- /usr/include/c++/14.1.1/tr1/modified_bessel_func.tcc \
- /usr/include/c++/14.1.1/tr1/poly_hermite.tcc \
- /usr/include/c++/14.1.1/tr1/poly_laguerre.tcc \
- /usr/include/c++/14.1.1/tr1/riemann_zeta.tcc
+ /usr/include/c++/14.1.1/bits/istream.tcc
--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,127 @@
-#include "../include/renderer.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <cmath>
 
-GLuint VBO, VAO, shaderProgram;
+const GLint WIDTH = 800, HEIGHT = 600;
 
-// Shader sources
-const char* vertexShaderSource = R"glsl(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    void main() {
-        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+GLfloat boxPosX = 0.0f;
+GLfloat boxPosY = 0.0f;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    const GLfloat moveSpeed = 0.1f;
+
+    if (key == GLFW_KEY_W && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        boxPosY += moveSpeed;
+    else if (key == GLFW_KEY_S && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        boxPosY -= moveSpeed;
+    else if (key == GLFW_KEY_A && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        boxPosX -= moveSpeed;
+    else if (key == GLFW_KEY_D && (action == GLFW_PRESS || action == GLFW_REPEAT))
+        boxPosX += moveSpeed;
+    else if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void render() {
+    glPushMatrix();
+    glTranslatef(boxPosX, boxPosY, 0.0f);
+
+    // Draw Cube
+    glBegin(GL_QUADS);
+
+    // Front face
+    glColor3f(1.0f, 0.0f, 0.0f); // Red
+    glVertex3f(-0.5f, -0.5f, 0.5f);
+    glVertex3f( 0.5f, -0.5f, 0.5f);
+    glVertex3f( 0.5f,  0.5f, 0.5f);
+    glVertex3f(-0.5f,  0.5f, 0.5f);
+
+    // Back face
+    glColor3f(0.0f, 1.0f, 0.0f); // Green
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glVertex3f(-0.5f,  0.5f, -0.5f);
+    glVertex3f( 0.5f,  0.5f, -0.5f);
+    glVertex3f( 0.5f, -0.5f, -0.5f);
+
+    // Left face
+    glColor3f(0.0f, 0.0f, 1.0f); // Blue
+    glVertex3f(-0.5f, -0.5f,  0.5f);
+    glVertex3f(-0.5f,  0.5f,  0.5f);
+    glVertex3f(-0.5f,  0.5f, -0.5f);
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+
+    // Right face
+    glColor3f(1.0f, 1.0f, 0.0f); // Yellow
+    glVertex3f( 0.5f, -0.5f, -0.5f);
+    glVertex3f( 0.5f,  0.5f, -0.5f);
+    glVertex3f( 0.5f,  0.5f,  0.5f);
+    glVertex3f( 0.5f, -0.5f,  0.5f);
+
+    // Top face
+    glColor3f(1.0f, 0.0f, 1.0f); // Magenta
+    glVertex3f(-0.5f,  0.5f,  0.5f);
+    glVertex3f( 0.5f,  0.5f,  0.5f);
+    glVertex3f( 0.5f,  0.5f, -0.5f);
+    glVertex3f(-0.5f,  0.5f, -0.5f);
+
+    // Bottom face
+    glColor3f(0.0f, 1.0f, 1.0f); // Cyan
+    glVertex3f(-0.5f, -0.5f, -0.5f);
+    glVertex3f( 0.5f, -0.5f, -0.5f);
+    glVertex3f( 0.5f, -0.5f,  0.5f);
+    glVertex3f(-0.5f, -0.5f,  0.5f);
+
+    glEnd();
+
+    glPopMatrix();
+}
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Error al inicializar GLFW\n";
+        return -1;
     }
-)glsl";
 
-const char* fragmentShaderSource = R"glsl(
-    #version 330 core
-    out vec4 FragColor;
-    void main() {
-        FragColor = vec4(1.0, 0.5, 0.2, 1.0); // Color naranja
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "MyOpenGLGame", NULL, NULL);
+    if (!window) {
+        std::cerr << "Error al crear la ventana GLFW\n";
+        glfwTerminate();
+        return -1;
     }
-)glsl";
 
-void setupShaders() {
-    // Compilar shaders
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
+    glfwMakeContextCurrent(window);
 
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Error al inicializar GLEW\n";
+        return -1;
+    }
 
-    // Vincular shaders
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
+    glfwSetKeyCallback(window, key_callback);
 
-    // Liberar shaders
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Set up projection matrix
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+
+        // Set up view matrix
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(0.0f, 0.0f, 5.0f, // Eye position
+                  0.0f, 0.0f, 0.0f, // Look-at position
+                  0.0f, 1.0f, 0.0f); // Up direction
+
+        render();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
-
-void createSquare() {
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,  // Abajo izquierda
-         0.5f, -0.5f, 0.0f,  // Abajo derecha
-         0.5f,  0.5f, 0.0f,  // Arriba derecha
-        -0.5f,  0.5f, 0.0f   // Arriba izquierda
-    };
-
-    unsigned int indices[] = {
-        0, 1, 2,   // Primer triángulo
-        2, 3, 0    // Segundo triángulo
-    };
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Configurar atributos de vértice
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // Configurar el buffer de índices
-    GLuint EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Desvincular VBO y VAO
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-}
-
-void renderSquare(GLFWwindow* window, float posX, float posY) {
-    glUseProgram(shaderProgram);
-
-    // Transformación para mover el cuadrado
-    glm::mat4 model = glm::mat4(1.0f);  // Inicializar como matriz identidad
-    model = glm::translate(model, glm::vec3(posX, posY, 0.0f));
-
-    // Obtener la ubicación de la matriz de modelo en el shader
-    unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-    // Dibujar el cuadrado
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
-}
-
